79e53d814ce00c6e102a01e51683358716e131c6
Pepppa9351 <javstyyay@gmail.com>
2025-07-10 18:06:42 +0200

    product page update

--- js/product-page.js
+++ js/product-page.js
@@ -17,8 +17,8 @@ function OnLoad() {
 }
 
 function displayProduct(product) {
-  document.getElementById('product-name').textContent = product.name;
   document.getElementById('product-image').src = product.image;
+  document.getElementById('product-name').textContent = product.name;
   document.getElementById('product-price').textContent = product.price + " Kč";
 }
 
